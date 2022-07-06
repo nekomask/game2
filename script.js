a6ac71e1cda29ff6ab70bdc4fbba11d657bb3958
kittenmasque <volaju@gmail.com>
2022-07-05 23:55:57 -0700

    placed white hole over scrolling block

---
+++ script.js
@@ -0,0 +1,8 @@
+let block = document.getElementById("block");
+let hole = document.getElementById("hole")
+
+hole.addEventListener('animationiteration', () => {
+    let random = -((Math.random()*300) + 150);
+    hole.style.top = random + "px"
+
+});
